eaed1cafdf9a7ea4bf650f43dbefdcb2e3a5ea7f
Keaton <keaton.arneson@gmail.com>
2022-01-12 10:58:25 -0600

    Name changes

--- components/Header.jsx
+++ components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
-              The Process
+              KnK Blog
             </span>
           </Link>
         </div>
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -22,6 +22,10 @@ const PostDetails = ({ post }) => {
 
   return (
     <>
+      <Head>
+        <title>{post.title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <div className="container mx-auto px-10 mb-8">
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
           <div className="col-span-1 lg:col-span-8">
